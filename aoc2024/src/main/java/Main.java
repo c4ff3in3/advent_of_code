34931998f7127b192772998aaf8062d57ac00927
Jacob Ogle <jacobogle@proton.me>
2024-12-04 14:38:58 -0500

    main

--- aoc2024/src/main/java/Main.java
+++ aoc2024/src/main/java/Main.java
@@ -1,14 +1,6 @@
-import common.InputReader;
-
-import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
-        try {
-            var io = InputReader.readInput("one");
-            System.out.println(io);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+        System.out.println("Advent of Code 2024 🎄");
     }
 }
